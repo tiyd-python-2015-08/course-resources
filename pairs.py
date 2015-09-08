e38cccc9cb7d9d4411c24649f78fb76a0b844811
James M. Allen <james.m.allen@gmail.com>
2015-09-08 14:36:56 -0400

    Classroom tools - written in class

---
+++ pairs.py
@@ -0,0 +1,8 @@
+from pydur import students, get_random_pairs
+
+def main():
+    print('\n'.join([' & '.join(pair) for pair in get_random_pairs(students)]))
+
+
+if __name__ == '__main__':
+    main()
---
+++ pydur.py
@@ -0,0 +1,39 @@
+import random
+
+students = '''Robert Amand
+Ryan Burton
+William Butts
+David Denney
+Jonathan Frederick
+Adam Hartz
+Glenn Hurley
+Karthik Kasala
+Jermaine Kee
+Michael Krawiec
+Tyler Kotkin
+Andrew Pierce
+Kathleen Rauh
+Brent Hilsabeck'''
+
+# turn into a list
+students = students.split('\n')
+
+def get_pairings(students):
+    return list({
+        frozenset((s1, s2))
+        for s1 in students
+        for s2 in students
+        if s1 != s2
+    })
+
+
+def get_random_pairs(students):
+    ret = []
+    pairings = get_pairings(students)
+    while len(pairings) > 0:
+        this_pair = random.choice(pairings)
+        ret.append(this_pair)
+        for pair in pairings[:]:
+            if not this_pair.isdisjoint(pair):
+                pairings.remove(pair)
+    return ret
